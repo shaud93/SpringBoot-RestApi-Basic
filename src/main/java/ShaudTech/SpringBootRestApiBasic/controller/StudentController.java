6a8782ffebf8d072985c170efbc7d53ab59399d5
Deante Watson <shaudwatson93@gmail.com>
2023-07-05 19:50:12 -0400

    added path variable and request variable examples

--- src/main/java/ShaudTech/SpringBootRestApiBasic/controller/StudentController.java
+++ src/main/java/ShaudTech/SpringBootRestApiBasic/controller/StudentController.java
@@ -2,6 +2,8 @@ package ShaudTech.SpringBootRestApiBasic.controller;
 
 import ShaudTech.SpringBootRestApiBasic.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,6 +17,8 @@ public class StudentController {
         Student student = new Student(1, "Dante", "watson");
         return student;
     }
+
+    // Get List Example
     @GetMapping("students")
     public List<Student> getStudents(){
         List<Student> students = new ArrayList<>();
@@ -25,4 +29,21 @@ public class StudentController {
         students.add(new Student(5,"lara", "croft"));
         return students;
     }
+    // Path Variable Example
+    @GetMapping("students/{id}/{f-name}/{l-name}")
+    public Student studentPathVariable(@PathVariable("id") int studentId,
+                                       @PathVariable("f-name") String firstName,
+                                       @PathVariable("l-name") String lastName){
+        return new Student(studentId, firstName, lastName);
+    }
+    // Query/Request param Example
+    // http://localhost:8080/students/query?id=1&firstName=dante&lastName=watson
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName) {
+        return new Student(id, firstName, lastName);
+    }
+    // PostMapping And RequestMapping
+
 }
