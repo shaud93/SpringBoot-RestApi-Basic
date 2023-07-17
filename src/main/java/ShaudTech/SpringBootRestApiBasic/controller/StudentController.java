f6330690f9cf9aacac97c642f37156448e5e6e46
Deante Watson <shaudwatson93@gmail.com>
2023-07-16 22:43:18 -0400

    added PutMapping

--- src/main/java/ShaudTech/SpringBootRestApiBasic/controller/StudentController.java
+++ src/main/java/ShaudTech/SpringBootRestApiBasic/controller/StudentController.java
@@ -1,10 +1,8 @@
 package ShaudTech.SpringBootRestApiBasic.controller;
 
 import ShaudTech.SpringBootRestApiBasic.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,5 +43,13 @@ public class StudentController {
         return new Student(id, firstName, lastName);
     }
     // PostMapping And RequestMapping
-
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student)
+    {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
 }
